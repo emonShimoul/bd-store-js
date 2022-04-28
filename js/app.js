c9e76eca5a22286992716f84f437eee4c7cf3d9b
emonShimoul <emonshimoul@gmail.com>
2022-04-28 10:27:02 +0600

    grandTotal update function

--- js/app.js
+++ js/app.js
@@ -78,3 +78,9 @@ const updateTaxAndCharge = () => {
   }
 };
 
+//grandTotal update function
+const updateTotal = () => {
+  const grandTotal = getInputValue("price") + getInputValue("delivery-charge") + getInputValue("total-tax");
+  document.getElementById("total").innerText = parseFloat(grandTotal.toFixed(2));
+};
+
