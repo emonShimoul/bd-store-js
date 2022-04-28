b1e1e4e2242d503a278ef7f1e0744038d6eba906
emonShimoul <emonshimoul@gmail.com>
2022-04-28 09:31:55 +0600

    load products from api

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const loadProducts = () => {
+  const url = `https://fakestoreapi.com/products`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showProducts(data));
+};
+loadProducts();
